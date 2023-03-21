4496a8cd4e9819b79ac36e3d6593d894895a8dcc
Harper Rhett <harperrhett@gmail.com>
2023-03-20 19:11:47 -0500

    Random process order

--- Scripts/Cloth.cs
+++ Scripts/Cloth.cs
@@ -11,6 +11,7 @@ public partial class Cloth : Node2D
 	public List<Connection> connections = new List<Connection>();
 	public bool simulationPaused = false;
 	public bool visualizeStress = false;
+	public Random rng = new Random();
 
 	// Settings
 	private const int Separation = 50;
@@ -29,16 +30,30 @@ public partial class Cloth : Node2D
 
 	// Add a connection
 	public Connection AddConnection(Connection connection) {
-		AddChild(connection);
+		// Add to list
 		connections.Add(connection);
+
+		// Put into tree
+		AddChild(connection);
+		int treePosition = rng.Next(joints.Count, joints.Count + connections.Count);
+		MoveChild(connection, treePosition);
+
+		// Return connection
 		return connection;
 	}
 
 	// Add a joint
 	public Joint AddJoint(Vector2 position, bool isFixed) {
+		// Create joint and add to list
 		Joint newJoint = new Joint(this, position, isFixed);
 		joints.Add(newJoint);
+
+		// Put into tree
 		AddChild(newJoint);
+		int treePosition = rng.Next(0, joints.Count);
+		MoveChild(newJoint, treePosition);
+
+		// Return joint
 		return newJoint;
 	}
 
