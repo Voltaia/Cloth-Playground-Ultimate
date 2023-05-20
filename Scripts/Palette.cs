6b3118a669f672959b50feffc0d5758b3d25313d
Harper Rhett <harperrhett@gmail.com>
2023-05-20 17:52:57 -0400

    Added two palettes

--- Scripts/Palette.cs
+++ Scripts/Palette.cs
@@ -14,25 +14,41 @@ public static class Palette
 	// Palette variations
 	public enum Theme {
 		Classic,
-		Marine
+		Marine,
+		Crimson,
+		Grayscale
 	}
 
 	// Set palette
 	public static void Set(Theme theme) {
 		switch (theme) {
 			default:
-			backgroundColor = Colors.DimGray;
-			jointColor = Colors.Blue;
-			fixedJointColor = Colors.DarkBlue;
-			connectionColor = Colors.LightBlue;
-			break;
+				backgroundColor = Colors.DimGray;
+				jointColor = Colors.Blue;
+				fixedJointColor = Colors.DarkBlue;
+				connectionColor = Colors.LightBlue;
+				break;
 
 			case Theme.Marine:
-			backgroundColor = new Color("060608");
-			jointColor = new Color("285cc4");
-			fixedJointColor = new Color("143464");
-			connectionColor = new Color("6d758d");
-			break;
+				backgroundColor = new Color("060608");
+				jointColor = new Color("285cc4");
+				fixedJointColor = new Color("143464");
+				connectionColor = new Color("6d758d");
+				break;
+			
+			case Theme.Crimson:
+				backgroundColor = new Color("090A0E");
+				jointColor = new Color("880C25");
+				fixedJointColor = new Color("2f040d");
+				connectionColor = new Color("F05C79");
+				break;
+
+			case Theme.Grayscale:
+				backgroundColor = new Color("000000");
+				jointColor = new Color("666666");
+				fixedJointColor = new Color("222222");
+				connectionColor = new Color("cccccc");
+				break;
 		}
 	}
 }
